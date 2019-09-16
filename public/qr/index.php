8c1a8fcd49e006daf9551932395990004e914bdc
Manuel Cerón <mcems7@gmail.com>
2019-09-16 23:43:19 +0000

    make it better

--- public/qr/index.php
+++ public/qr/index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>QR</title>
     <script type="text/javascript" src="lib/html5_qrcode.min.js"></script>
-    <!--script type="text/javascript" src="lib/html5_qrcode.min.js"></script-->
-    <!--script type="text/javascript" src="src/html5_qrcode.min.js"></script-->
+    <script type="text/javascript" src="lib/html5_qrcode.min.js"></script>
+    <script type="text/javascript" src="src/html5_qrcode.min.js"></script>
 </head>
 <body>
      <div id="reader" style="width:300px;height:250px"></div>
@@ -18,8 +18,9 @@
   integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
   crossorigin="anonymous"></script>
 <script>
-     $('#reader').html5_qrcode(function(data){
- 		 // do something when code is read
+    $('#reader').html5_qrcode(function(data){
+        alert(data);
+ 		 //do something when code is read
  	},
  	function(error){
 		//show read errors 
